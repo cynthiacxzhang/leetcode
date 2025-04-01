59269d069eeedfbae93581e9c7e4afb0f3974ef5
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-03-31 23:06:23 -0400

    des

--- easy/twosum.py
+++ easy/twosum.py
@@ -1,4 +1,6 @@
 # Twosum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 class Solution(object):
     def twoSum(self, nums, target):
         """

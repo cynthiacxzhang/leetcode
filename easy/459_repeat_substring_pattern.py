ca21d58bd730fc0503702d0eb491746a40ffd6df
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-05-05 20:25:20 -0400

    459 solution

--- easy/459_repeat_substring_pattern.py
+++ easy/459_repeat_substring_pattern.py
@@ -11,4 +11,14 @@ Explanation: It is the substring "ab" twice.
 """
 
 
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
 
+        # Solution premise: String Rotation (s+s) then remove first and last char should still preserve "s" in the middle of the new string
+        # - then check if s is contained within the "rotated" new string
+        
+        return s in (s + s)[1:-1]

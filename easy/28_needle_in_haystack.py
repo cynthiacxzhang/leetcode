75f719d3f65aa871ed7d0d173aaba4b89091147e
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-05-09 20:22:11 -0400

    28 solution

--- easy/28_needle_in_haystack.py
+++ easy/28_needle_in_haystack.py
@@ -4,4 +4,29 @@
 Given two strings needle and haystack, 
 return the index of the first occurrence of needle in haystack, 
 or -1 if needle is not part of haystack.
+
+Note: solved using Python method "find"
+- could also use "in", but that doesn't return the index
+- using .index() raises a valueerror if not found
 """
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        """
+        Brute force method: iterate through characters in the word that match first letter of needle
+        - 
+        """
+        # Solution: use Python "in" method
+        if needle in haystack:
+            index = haystack.find(needle)
+            return index
+        else:
+            return -1
+
+
+        

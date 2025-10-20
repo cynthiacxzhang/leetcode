da11a2b8812acf0010b4b22c1b3bf00cd9afe831
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-10-19 23:46:02 -0400

    53 solution

--- medium/53_max_subarray.py
+++ medium/53_max_subarray.py
@@ -1,8 +1,24 @@
-# max subarray of given input array nums
+# max subarray problem
 
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
+
+
+# brute force solution - nested loop with O(n^2) time complexity
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        # note: will need to move left pointer too
+
+        max_sum = nums[0]
+        n = len(nums)
         
-    }
-};
+        for i in range(0, n):
+            # resets to prevent loop carryover
+            curr_sum = 0
+
+            for j in range(i, n):
+                curr_sum += nums[j]
+                max_sum = max(max_sum, curr_sum)
+
+        return max_sum
+        
+# optimal solution - Kadane's algorithm with O(n) time complexity

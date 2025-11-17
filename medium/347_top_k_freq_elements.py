b9e058c3e357480eb8d4225d5f81fbe3b7774c05
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-11-17 10:34:37 -0500

    top k bucket sort 347

---
+++ medium/347_top_k_freq_elements.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        freq = {}
+
+        for num in nums: 
+            key = num
+            freq[key] = freq.get(num, 0) + 1
+        
+        # bucket sort method
+        # max frequency = length of array (assuming [1, 1, 1, 1])
+        # need arr len + 1 buckets (to include 0)
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        # push number into frequency location
+        for num, count in freq.items():
+            buckets[count].append(num)
+
+        output = []
+        for count in range(len(buckets)-1, -1, -1):
+            for num in buckets[count]:
+                output.append(num)
+                if len(output) == k:
+                    return output
+
+
--- medium/49_group_anagrams.py
+++ medium/49_group_anagrams.py
@@ -16,15 +16,24 @@ class Solution:
 
         dictionary = {anagram: [indexes with that anagram value]}
         """
-            
+        
         # map that stores key = sorted string -> val = array of indexes
         groups = {}
 
         # store all indexes with the same anagram under the same hash location
         for string in strs:
             # sort string 
-            key = "".join(sorted(string))
+            # key = "".join(sorted(string)) # sorting the string is nlogn, what is more optimaL?
             
+
+            # ascii frequency mapping
+            freq_count = [0]*26
+            for ch in string:
+                idx = ord(ch) - ord('a')
+                freq_count[idx] += 1
+
+            key = tuple(freq_count)
+
             # if key dne yet, initialize it
             if key not in groups:
                 groups[key] = []

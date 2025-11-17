3e26b6741836d056eb8aefccc21e1e04c2a2bbe0
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-11-17 13:58:37 -0500

    heap implementation of top k

--- medium/347_top_k_freq_elements.py
+++ medium/347_top_k_freq_elements.py
@@ -22,5 +22,27 @@ class Solution:
                 output.append(num)
                 if len(output) == k:
                     return output
+        
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
+                # heaps method
+        import heapq
 
+        freq = {} # (num:count)
+
+        for num in nums: 
+            key = num
+            freq[key] = freq.get(num, 0) + 1
+
+        heap = []
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        output = []
+        for count, num in heap:
+            output.append(num)
+        
+        return output

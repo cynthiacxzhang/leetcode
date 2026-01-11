8d3345e09be9d559d786a6d1463a4c613a939c9b
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2026-01-11 18:17:47 -0500

    53 kadane's algorithm

--- medium/53_max_subarray.py
+++ medium/53_max_subarray.py
@@ -1,13 +1,8 @@
-# max subarray problem
-
-from typing import List
-
-# brute force solution - nested loop with O(n^2) time complexity
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
 
-        # note: will need to move left pointer too
-
+        # brute force - all subarrays and sums - TLE
+        """
         max_sum = nums[0]
         n = len(nums)
         
@@ -20,5 +15,34 @@ class Solution:
                 max_sum = max(max_sum, curr_sum)
 
         return max_sum
+        """
+
+        # kadane's algorithm - smarter way of judging reset point
+        # if adding nums[i] makes curr_sum negative, it's better just to start over with the current x
+        # this rule holds unless the current x is words than curr_sum
+        # in which case, just append it and keep going
+
+        max_sum = nums[0]
+        curr_sum = 0
+
+        # pointers, rhs is implicit, lhs (j) is iterative
+        for j in range(len(nums)):
+            # if curr is negative and less than current value in array, reset
+            if curr_sum < 0 and curr_sum < nums[j]:
+                curr_sum = nums[j]
+
+            # else, add current value onto running sum
+            else:
+                curr_sum += nums[j]
+            
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
+
+
+
+
+
+
+
         
-# optimal solution - Kadane's algorithm with O(n) time complexity

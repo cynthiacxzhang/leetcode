f3a914558ca991963fe979564b97a89d65978dcf
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2026-01-11 17:07:17 -0500

    153 better soln

--- medium/153_min_rot_sort_arr.py
+++ medium/153_min_rot_sort_arr.py
@@ -2,6 +2,30 @@
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        """
         return min(nums)
+        """
 
+        class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        """
+        return min(nums)
+        """
+
+        # proper solution using binary search
+        l, r = 0, len(nums)-1
+
+        while l < r: # cannot overlap since rhs is being set at mid, and not mid - 1
+            mid = (l+r) // 2
+
+            if nums[r] < nums[mid]:
+                # min in rhs
+                l = mid + 1
+            else:
+                # min = mid or in lhs
+                r = mid
         
+        # l = mid = r when this happens
+        return nums[l]
+            
+

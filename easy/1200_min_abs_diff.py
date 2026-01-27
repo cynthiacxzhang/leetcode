d7b21da3171aa31840d4021cf6ca608805554fc7
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2026-01-26 19:03:39 -0500

    min abs difference

---
+++ easy/1200_min_abs_diff.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+
+        if not arr:
+            return 0
+        
+        arr.sort()
+        print(arr)
+        min_diff = arr[1] - arr[0] # placeholder
+
+        # frequency map and finding min diff
+        freq = {}
+
+        for i in range(0, len(arr)-1):
+            curr_diff = arr[i + 1] - arr[i]
+            min_diff = min(curr_diff, min_diff)
+
+            freq.setdefault(curr_diff, []).append([arr[i], arr[i+1]])
+        
+        return freq[min_diff]
+
+
+
+            
+        
+            
+

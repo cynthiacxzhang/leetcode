700a41e26e030f5ba2daa818bdbaf1df6186361d
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2026-02-02 08:03:16 -0500

    big countries

---
+++ easy/595_big_countries.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    
+    condition = (world["area"] >= 3_000_000) | (world["population"] >=25_000_000)
+
+    return world.loc[condition, ["name", "area", "population"]]

409bde8e3e4941775577032d206e9f65dd4d9292
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2026-02-24 23:00:53 -0500

    optimal

--- easy/1356_sort_int_by_bit.py
+++ easy/1356_sort_int_by_bit.py
@@ -1,24 +1,27 @@
 class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
+    # def sortByBits(self, arr: List[int]) -> List[int]:
 
-        new_arr = {}
-        for num in arr:
-            val = str(bin(num)[2:])
-            sum = 0
-            for ch in val:
-                if ch == '1':
-                    sum += 1
-                else:
-                    sum += 0
+    #     new_arr = {}
+    #     for num in arr:
+    #         val = str(bin(num)[2:])
+    #         sum = 0
+    #         for ch in val:
+    #             if ch == '1':
+    #                 sum += 1
+    #             else:
+    #                 sum += 0
 
-            new_arr.setdefault(sum, []).append(num)
-            new_arr[sum] = sorted(new_arr[sum])
+    #         new_arr.setdefault(sum, []).append(num)
+    #         new_arr[sum] = sorted(new_arr[sum])
         
-        print(new_arr)
+    #     print(new_arr)
 
-        final = []
-        for thing in sorted(new_arr):
-            final.extend(sorted(new_arr[thing]))
+    #     final = []
+    #     for thing in sorted(new_arr):
+    #         final.extend(sorted(new_arr[thing]))
 
+    #     return final
 
-        return final
+    # optimal solution
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key=lambda x: (x.bit_count(), x))

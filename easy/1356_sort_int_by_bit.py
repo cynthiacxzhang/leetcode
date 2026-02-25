30d1a9052a30e897e93fb70df1c51f3cbd887ce8
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2026-02-24 22:57:54 -0500

    brute force

---
+++ easy/1356_sort_int_by_bit.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+
+        new_arr = {}
+        for num in arr:
+            val = str(bin(num)[2:])
+            sum = 0
+            for ch in val:
+                if ch == '1':
+                    sum += 1
+                else:
+                    sum += 0
+
+            new_arr.setdefault(sum, []).append(num)
+            new_arr[sum] = sorted(new_arr[sum])
+        
+        print(new_arr)
+
+        final = []
+        for thing in sorted(new_arr):
+            final.extend(sorted(new_arr[thing]))
+
+
+        return final
